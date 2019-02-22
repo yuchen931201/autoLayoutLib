de4a82a6d806f49b71384837730e1a878d1c1a75
tianzhen <tianzhen@chelun.com>
2019-02-22 09:51:05 +0800

    -1

--- app/src/main/java/com/tz/autolayout/MainActivity.java
+++ app/src/main/java/com/tz/autolayout/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import com.tz.autolayoutlib.AutoScreen;
-import com.tz.autolayoutlib.AutoScreenJ;
 
 /**
  * @ComputerCode: YD-YF-2015083113-1
@@ -21,6 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AutoScreen.Companion.auto(this);
-//        AutoScreenJ.auto(this);
+        AutoScreenJ.auto(this);
     }
 }
